e916f083a6c6f69814921210cc1863631b535542
Anton Styazhkin <rudana089@gmail.com>
2023-03-09 14:37:01 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const galleryItemsLightbox = galleryItems
+    .map(({ preview, original, description }) => {
+        return `<li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>`;
+    })
+    .join('');
+
+gallery.innerHTML = galleryItemsLightbox;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
